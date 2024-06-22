a65dd08f45adf1e8acfced09cf7c1227c21dd0ab
Nikola Mitrovic <secerzema@Nikolas-MacBook-Air.local>
2024-06-22 10:18:53 +0200

    Chapter06 MSW bypass real requests.

--- Chapter06/custom-state-management/src/main.ts
+++ Chapter06/custom-state-management/src/main.ts
@@ -12,7 +12,9 @@ async function enableMocking() {
  
   // `worker.start()` returns a Promise that resolves
   // once the Service Worker is up and ready to intercept requests.
-  return worker.start()
+  return worker.start({
+    onUnhandledRequest: "bypass", // Optional: Let real requests pass through if not mocked
+  })
 }
 
 enableMocking().then(() => {
