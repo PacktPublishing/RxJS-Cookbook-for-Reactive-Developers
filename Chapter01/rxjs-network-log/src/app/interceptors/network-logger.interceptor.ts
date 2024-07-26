926bedbf016995e0b76b07c170594ebbf3648a3d
Nikola Mitrovic <secerzema@Nikolas-MacBook-Air.local>
2024-07-26 02:55:50 +0200

    Chapter01 Network logger refactor.

--- Chapter01/rxjs-network-log/src/app/interceptors/network-logger.interceptor.ts
+++ Chapter01/rxjs-network-log/src/app/interceptors/network-logger.interceptor.ts
@@ -6,23 +6,39 @@ import {
 } from '@angular/common/http';
 import { inject } from '@angular/core';
 import {
+  Observable,
   Subject,
   catchError, exhaustMap,
-  finalize, tap
+  finalize, of, retry, tap
 } from 'rxjs';
+import { RecipesService } from '../services/recipes.service';
 
 export const networkLoggerInterceptor: HttpInterceptorFn = (req, next) => {
   const started = Date.now();
   const httpClient = inject(HttpClient);
-  const errorSubject = new Subject<HttpErrorResponse>();
+  const recipeService = inject(RecipesService);
 
-  errorSubject
-    .pipe(
-      exhaustMap((error: HttpErrorResponse) =>
-        httpClient.post('https://super-recipes.com/api/analytics', error)
-      )
-    )
-    .subscribe();
+  function logSuccessfulResponse(event: any) {
+    if (event instanceof HttpResponse) {
+      const elapsed = Date.now() - started;
+      console.log(`Request took %c${elapsed} ms`, 'color: #ffc26e');
+      console.log('%cResponse:', 'color: #d30b8e', event);
+    }
+  }
+
+  function logFailedResponse(error: HttpErrorResponse): Observable<never> {
+    const elapsed = Date.now() - started;
+    console.log(
+      `Request for ${req.urlWithParams} failed after %c${elapsed} ms`,
+      'color: #ffc26e'
+    );
+    console.log('%cError:', 'color: #d30b8e', error);
+    recipeService.errorSubject.next(error);
+
+    return of();
+  }
+
+  
 
   return next(req).pipe(
     tap(() =>
@@ -31,23 +47,9 @@ export const networkLoggerInterceptor: HttpInterceptorFn = (req, next) => {
         `\nRequest for ${req.urlWithParams} started...`
       )
     ),
-    tap((event) => {
-      if (event instanceof HttpResponse) {
-        const elapsed = Date.now() - started;
-        console.log(`Request took %c${elapsed} ms`, 'color: #ffc26e');
-        console.log('%cResponse:', 'color: #d30b8e', event);
-      }
-    }),
-    catchError((error) => {
-      const elapsed = Date.now() - started;
-      console.log(
-        `Request for ${req.urlWithParams} failed after %c${elapsed} ms`,
-        'color: #ffc26e'
-      );
-      console.log('%cError:', 'color: #d30b8e', error);
-      errorSubject.next(error);
-      throw error;
-    }),
+    tap((event) => logSuccessfulResponse(event)),
+    catchError((error) => logFailedResponse(error)),
+    retry(5),
     finalize(() => console.log('---------------------------------'))
   );
 };
--- Chapter01/rxjs-network-log/src/app/services/recipes.service.ts
+++ Chapter01/rxjs-network-log/src/app/services/recipes.service.ts
@@ -1,14 +1,23 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject, exhaustMap } from 'rxjs';
 import { Recipe } from '../types/recipes.type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipesService {
+  public errorSubject = new Subject<HttpErrorResponse>();
   
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.errorSubject
+    .pipe(
+      exhaustMap((error: HttpErrorResponse) =>
+        this.http.post('https://super-recipes.com/api/analytics', error)
+      )
+    )
+    .subscribe();
+  }
 
   getRecipes$(): Observable<Recipe[]> {
    return this.http.get<Recipe[]>('https://super-recipes.com/api/recipes');
