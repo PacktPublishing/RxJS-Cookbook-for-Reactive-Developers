2a5b0ac84085a309826dbd14780cf6be4b5e4ad2
Nikola Mitrovic <secerzema@Nikolas-MacBook-Air.local>
2024-08-19 00:41:18 +0200

    Chapter01 - Network polling - add new recipe after third request and refresh recipe list.

--- Chapter01/network-polling/src/mocks/handlers.ts
+++ Chapter01/network-polling/src/mocks/handlers.ts
@@ -1,9 +1,35 @@
 import { delay, http, HttpResponse } from 'msw'
 import { recipes } from './mock.json'
 
+let requestNumber = 0;
+
 export const handlers = [
   http.get('https://super-recipes.com/api/recipes', async () => {
-    await delay(7000);
+    requestNumber++;
+    await delay(2000);
+
+    if (requestNumber === 3) {
+      return HttpResponse.json([
+        ...recipes,
+        {
+          "id": 6,
+          "name": "Chicken Fajitas",
+          "description": "Sizzling Mexican dish with chicken, peppers, onions, and tortillas.",
+          "ingredients": [
+            "chicken breasts",
+            "bell peppers",
+            "onion",
+            "fajita seasoning",
+            "lime juice",
+            "tortillas",
+            "guacamole",
+            "sour cream",
+            "salsa"
+          ],
+          "image": "/assets/images/chicken_fajitas.png"
+        }
+      ]);
+    }
 
     return HttpResponse.json(recipes);
   }),
