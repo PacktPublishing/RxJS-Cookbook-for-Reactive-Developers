451aac5ccaf3861285eebc831cd58e861d264b03
Nikola Mitrovic <secerzema@Nikolas-MacBook-Air.local>
2024-08-23 18:50:45 +0200

    Chapter07 - Angular custom router navigation.

--- Chapter07/rxjs-custom-router/src/app/app.component.ts
+++ Chapter07/rxjs-custom-router/src/app/app.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterService } from './services/router.service';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'rxjs-custom-router';
+  component: any;
+
+  constructor(private routerService: RouterService) { }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.routerService.navigate('/home');
+    }, 5000);
+    setTimeout(() => {
+      this.routerService.navigate('/about');
+    }, 15000);
+    this.routerService.getCurrentRoute().subscribe(route => {
+      console.log('Current route:', route);
+      switch (route?.path) {
+        case '/home':
+          this.component = HomeComponent;
+          break;
+        case '/about':
+          this.component = AboutComponent;
+          break;
+        // ... more routes here ...
+      }
+    });
+  }
 }
--- Chapter07/rxjs-custom-router/src/app/app.routes.ts
+++ Chapter07/rxjs-custom-router/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    }
+];
