e4abe679eab60705b7c9172f0a3a1bf5ffe4bac2
Nikola Mitrovic <secerzema@Nikolas-MacBook-Air.local>
2024-09-04 20:04:25 +0200

    Chapter09 - Offline cache first - refactoring.

--- Chapter09/rxjs-offline-cache-first/src/app/interceptors/offline.interceptor.ts
+++ Chapter09/rxjs-offline-cache-first/src/app/interceptors/offline.interceptor.ts
@@ -5,38 +5,35 @@ import {
   HttpRequest,
   HttpResponse,
 } from '@angular/common/http';
-import { EMPTY, catchError, filter, from, map, switchMap, tap } from 'rxjs';
+import { catchError, from, map, switchMap, withLatestFrom } from 'rxjs';
 
 export const offlineInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
   const openCache$ = from(caches.open('my-app-cache'));
-  const continueRequest$ = next(req).pipe(
-    map((response: HttpEvent<unknown>) => {
-      if (response instanceof HttpResponse) {
-        return new HttpResponse({ status: 200, body: response.body });
-      }
+  const continueRequest$ = next(req);
 
-      return response;
-    })
-  );
+  if (navigator.onLine) {
+    return continueRequest$.pipe(
+      withLatestFrom(openCache$),
+      map(([response, cache]) => {
+        if (response instanceof HttpResponse) {
+          cache.put(req.url, new Response(JSON.stringify(response.body)));
+        }
+
+        return response;
+      })
+    );
+  }
 
   return openCache$.pipe(
     switchMap((cache: Cache) => {
-      if (!cache) {
-        return continueRequest$;
-      }
+      if (!cache) return continueRequest$;
 
       return from(cache.match(req.url)).pipe(
         switchMap((cacheValue: any) => cacheValue.json()),
         map((response: unknown) => new HttpResponse({ status: 200, body: response })),
         catchError(() => {
-          // No cached response found, go to network if online
-          return continueRequest$.pipe(
-            tap((response: HttpEvent<unknown>) => {
-              if (response instanceof HttpResponse) {
-                cache!.put(req.url, new Response(JSON.stringify(response.body)));
-              }
-            })
-          );
+          // No cached response found, go to network
+          return continueRequest$;
         })
       );
     })
