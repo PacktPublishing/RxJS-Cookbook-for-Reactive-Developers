1b1976cc15309d106435b98a55458a608f20b7d1
Nikola Mitrovic <secerzema@Nikolas-MacBook-Air.local>
2024-09-09 19:29:16 +0200

    Chapter09 - Optimistic update - convert to PWA.

--- Chapter09/rxjs-optimistic-update/src/app/app.config.ts
+++ Chapter09/rxjs-optimistic-update/src/app/app.config.ts
@@ -1,10 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideHttpClient(), provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
 };
