4db479b7f56db1bf68ee24a06abd197c2d19db77
Nikola Mitrovic <secerzema@Nikolas-MacBook-Air.local>
2024-09-20 22:33:46 +0200

    Chapter02 - Pro img - refactor.

--- Chapter02/rxjs-pro-img/src/app/components/pro-img/pro-img.component.ts
+++ Chapter02/rxjs-pro-img/src/app/components/pro-img/pro-img.component.ts
@@ -40,45 +40,55 @@ export class ProImgComponent implements OnInit {
     img.src = !this.src.startsWith('http')
       ? `assets/images/${this.src}`
       : this.src;
-      const loadImageWithProgress = (url: string): Observable<string> => {
-        return new Observable((observer) => {
-          const xhr = new XMLHttpRequest();
-      
-          xhr.open('GET', url, true);
-          xhr.responseType = 'blob';
-      
-          xhr.onprogress = (event) => {
-            if (event.lengthComputable) {
-              const percentComplete = Math.round((event.loaded / event.total) * 100);
-              this.loadingProgress$.next(+percentComplete);
-            }
-          };
-      
-          xhr.onload = () => {
-            const blobUrl = URL.createObjectURL(xhr.response);
-            observer.next(blobUrl);
-            observer.complete();
-          };
-      
-          xhr.onerror = () => {
-            this.loadingProgress$.next(-1);
-            observer.error(new Error('Image load failed'));
-          };
-      
-          xhr.send();
-        });
-      };
-      
 
-    // const loadComplete$ = fromEvent(img, 'load').pipe(map(() => 100)); // Emit 100 on load
-    // const loadError$ = fromEvent(img, 'error').pipe(map(() => -1)); // Emit -1 on error
+      // Track actual download progress of an image
+      // const loadImageWithProgress$ = (url: string): Observable<string> => {
+      //   return new Observable((observer) => {
+      //     const xhr = new XMLHttpRequest();
+      
+      //     xhr.open('GET', url, true);
+      //     xhr.responseType = 'blob';
+      
+      //     xhr.onprogress = (event) => {
+      //       if (event.lengthComputable) {
+      //         const percentComplete = Math.round((event.loaded / event.total) * 100);
+      //         this.loadingProgress$.next(+percentComplete);
+      //       }
+      //     };
+      
+      //     xhr.onload = () => {
+      //       const blobUrl = URL.createObjectURL(xhr.response);
+      //       observer.next(blobUrl);
+      //       observer.complete();
+      //     };
+      
+      //     xhr.onerror = () => {
+      //       this.loadingProgress$.next(-1);
+      //       observer.error(new Error('Image load failed'));
+      //     };
+      
+      //     xhr.send();
+      //   });
+      // };
+      // this.imageSrc$ = loadImageWithProgress$(img.src).pipe(
+      //   auditTime(1000),
+      //   startWith(placeholderImg.src),
+      //   takeWhile((src) => src === placeholderImg.src, true),
+      //   catchError(() => of(placeholderImg.src)),
+      //   shareReplay(1)
+      // );
 
-    this.imageSrc$ = loadImageWithProgress(img.src).pipe(
-      auditTime(1000),
-      startWith(placeholderImg.src),
-      takeWhile((src) => src === placeholderImg.src, true),
-      catchError(() => of(placeholderImg.src)),
-      shareReplay(1)
-    );
+    const loadProgress$ = timer(0, 100); 
+    const loadComplete$ = fromEvent(img, 'load').pipe(map(() => 100)); // Emit 100 on load
+    const loadError$ = fromEvent(img, 'error').pipe(map(() => -1)); // Emit -1 on error
+
+    this.imageSrc$ = merge(loadProgress$, loadComplete$, loadError$).pipe( 
+      tap((progress) => this.loadingProgress$.next(progress)), 
+      map((progress) => (progress === 100 ? img.src : placeholderImg.src)), 
+      startWith(placeholderImg.src), 
+      takeWhile((src) => src === placeholderImg.src, true), 
+      catchError(() => of(placeholderImg.src)), 
+      shareReplay(1) 
+    ); 
   }
 }
