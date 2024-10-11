8e8858958c022b46725d72ed51df99068e15937d
Nikola Mitrovic <secerzema@Nikolas-MacBook-Air.local>
2024-10-11 22:33:10 +0200

    Chapter10 - tic tac toe - styling, refactoring.

--- Chapter10/rxjs-tic-tac-toe/client/src/app/components/game-board/game-board.component.ts
+++ Chapter10/rxjs-tic-tac-toe/client/src/app/components/game-board/game-board.component.ts
@@ -1,37 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { GameService } from '../../services/game.service';
-import { WebSocketService, WsMessage } from '../../services/web-socket.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { GameService, WsMessage } from '../../services/game.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-game-board',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './game-board.component.html',
   styleUrl: './game-board.component.scss'
 })
-export class GameBoardComponent implements OnInit {
+export class GameBoardComponent implements OnInit, OnDestroy {
   currentPlayerTurn = 'X';
   playerJoined = '';
   board = Array(9).fill(null);
-  winner = '';
+  winner$!: Observable<'X' | 'O'>;
 
-  constructor(private wsService: WebSocketService) { }
+  constructor(private gameService: GameService) { }
 
   ngOnInit() {
-    this.wsService.getPlayers$().subscribe(({ data }: WsMessage) => {
+    this.gameService.getPlayers$().subscribe(({ data }: WsMessage) => {
       this.playerJoined = data;
     });
-    this.wsService.getWinner$().subscribe(({ data }: WsMessage) => {
-      this.winner = data;
-    });
-    this.wsService.getBoardUpdate$().subscribe(({ data }: WsMessage) => {
+    this.gameService.getBoardUpdate$().subscribe(({ data }: WsMessage) => {
       const { move, currentPlayer, nextPlayer } = data;
       this.board[move] = currentPlayer;
       this.currentPlayerTurn = nextPlayer;
     });
+    this.winner$ = this.gameService.getWinner$();
+  }
+
+  ngOnDestroy(): void {
+    this.gameService.close();
   }
 
   handleMove(field: number) {
-    this.wsService.move(field, this.playerJoined, this.currentPlayerTurn);
+    this.gameService.move(field, this.playerJoined, this.currentPlayerTurn);
+  }
+
+  isPlayerX(field: number): boolean {
+    return this.board[field] === 'X';
   }
 }
--- Chapter10/rxjs-tic-tac-toe/client/src/app/services/game.service.ts
+++ Chapter10/rxjs-tic-tac-toe/client/src/app/services/game.service.ts
@@ -1,33 +1,75 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { WebSocketService } from './web-socket.service';
+import { map } from 'rxjs';
+import { Observable } from 'rxjs/internal/Observable';
+import { WebSocketSubject, webSocket } from 'rxjs/webSocket';
+
+export interface WsMessage {
+  event: string;
+  data?: any;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
-  public winner = new BehaviorSubject<string>('');
-  public currentPlayerTurn = new BehaviorSubject<string>('X');
-  public playerJoined = new BehaviorSubject<string>('');
 
-  constructor(private webSocketService: WebSocketService) { }
+  private socket$!: WebSocketSubject<WsMessage>;
+  public playerJoined$!: Observable<WsMessage>;
+  public boardUpdate$!: Observable<WsMessage>;
+  public winner$!: Observable<WsMessage>;
 
-  join(): void {
-    this.webSocketService.send({ type: 'join' });
+  constructor() {
+    this.socket$ = webSocket<WsMessage>({
+      url: 'ws://localhost:8080',
+      deserializer: (e) => JSON.parse(e.data) as WsMessage,
+    });
+    this.playerJoined$ = this.socket$.multiplex(
+      () => ({ subscribe: 'join' }), 
+      () => ({ unsubscribe: 'join' }), 
+      (message) => message.event === 'join'
+    );
+    this.boardUpdate$ = this.socket$.multiplex(
+      () => ({ subscribe: 'boardUpdate' }), 
+      () => ({ unsubscribe: 'boardUpdate' }), 
+      (message) => message.event === 'boardUpdate'
+    );
+    this.winner$ = this.socket$.multiplex(
+      () => ({ subscribe: 'winner' }), 
+      () => ({ unsubscribe: 'winner' }), 
+      (message) => message.event === 'winner'
+    );
   }
 
-  move(field: number): void {
-    if (this.currentPlayerTurn.value !== this.playerJoined.value || this.winner.value) {
+  getPlayers$() {
+    return this.playerJoined$;
+  }
+
+  getWinner$(): Observable<'X' | 'O'> {
+    return this.winner$.pipe(map(({ data }: WsMessage) => data));
+  }
+
+  getBoardUpdate$() {
+    return this.boardUpdate$;
+  }
+
+  public send(message: any): void {
+    this.socket$.next(message);
+  }
+
+  public close(): void {
+    this.socket$.complete();
+  }
+
+  public get messages$() {
+    return this.socket$.asObservable();
+  }
+
+  move(field: number, player: string, currentPlayerTurn: string): void {
+    if (player !== currentPlayerTurn) {
       return;
     }
-    console.log('turn', this.currentPlayerTurn.value);
 
-    this.webSocketService.send({ type: 'move', message: {
-      field,
-      player: this.currentPlayerTurn.value
-    }}); 
+    this.send({ event: 'move', data: field }); 
   }
-
-
-
+  
 }
