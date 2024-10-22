854d048070316f84a5b4ba69d79ba737f9deea1d
Nikola Mitrovic <secerzema@Nikolas-MacBook-Air.local>
2024-10-22 03:46:21 +0200

    Chapter09 - Optimistic update - configurable retry config.

--- Chapter09/rxjs-optimistic-update/client/src/app/operators/optimistic-update.operator.ts
+++ Chapter09/rxjs-optimistic-update/client/src/app/operators/optimistic-update.operator.ts
@@ -3,17 +3,15 @@ import { catchError, ignoreElements, retry } from 'rxjs/operators';
 
 export function optimisticUpdate<T, E = any>(
   originalValue: T,
-  rollback: (value: T, error: E) => void
+  rollback: (value: T, error: E) => void,
+  retryConfig: { count: number; delay: number } = { count: 1, delay: 3000 }
 ): (source: Observable<T>) => Observable<T> {
   return (source: Observable<T>) => {
     return concat(
       of(originalValue),
       source.pipe(
         ignoreElements(),
-        retry({
-          count: 1,
-          delay: 3000,
-        }),
+        retry(retryConfig),
         catchError((error) => {
           rollback(originalValue, error);
           return throwError(() => error);
