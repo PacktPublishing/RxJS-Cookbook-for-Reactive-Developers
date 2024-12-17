983163c23b9632c39037477b1980bcf51b628ab1
Nikola Mitrovic <nikola.mitrovic1313@gmail.com>
2024-12-17 11:08:58 +0100

    Chapter10 - rxjs chat app - websocket reconnection logic.

--- Chapter10/rxjs-recipes-chat/client/src/app/services/chat.service.ts
+++ Chapter10/rxjs-recipes-chat/client/src/app/services/chat.service.ts
@@ -1,51 +1,104 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, catchError, delay, map, Observable, of, ReplaySubject, retry, scan } from 'rxjs';
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 
-export interface Message {
-  id: string;
-  message: string;
-  clientId: string;
-  timestamp: Date;
+export interface IWsMessage<T> {
+  event: string;
+  data: T;
 }
 
-export interface WsMessage {
-  event: string;
-  data?: any;
+export interface IMessage {
+  id?: string;
+  message: string;
+  clientId: string;
+  timestamp?: Date;
+  topic: string;
+}
+
+export interface IChatEvent {
+  clientId: string;
+  isTyping: boolean;
+}
+
+export interface IChatConnection {
+  clientId: string;
+  name: string;
+  otherClientId: string;
+  isOnline: boolean;
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
-  private socket$: WebSocketSubject<WsMessage>;
-  public chat$!: Observable<WsMessage>;
-  public clientConnection$!: Observable<WsMessage>;
-  public isTyping$!: Observable<WsMessage>;
+  private socket$!: WebSocketSubject<IWsMessage<any>>;
+  private socketOfflineMessages$ = new ReplaySubject<IWsMessage<IMessage>>(100);
+  public chat$!: Observable<IWsMessage<IMessage[]>>;
+  public clientConnection$!: Observable<IWsMessage<IChatConnection>>;
+  public isTyping$!: Observable<IWsMessage<IChatEvent>>;
 
   constructor() {
-    this.socket$ = webSocket<WsMessage>({
-      url: 'ws://localhost:8080',
-      deserializer: (e) => JSON.parse(e.data) as WsMessage,
-    });
-    this.chat$ = this.socket$.multiplex(
-      () => ({ subscribe: 'chat' }), 
-      () => ({ unsubscribe: 'chat' }), 
-      (message) => message.event === 'chat'
-    );
-    this.clientConnection$ = this.socket$.multiplex(
-      () => ({ subscribe: 'connect' }), 
-      () => ({ unsubscribe: 'connect' }), 
-      (message) => message.event === 'connect'
-    );
-    this.isTyping$ = this.socket$.multiplex(
-      () => ({ subscribe: 'typing' }), 
-      () => ({ unsubscribe: 'typing' }), 
-      (message) => message.event === 'typing'
-    );
+    this.connect();
+  }
+
+  connect() {
+    if (!this.socket$ || this.socket$.closed) {
+      this.socket$ = webSocket<IWsMessage<IMessage>>({
+        url: 'ws://localhost:8080',
+        deserializer: (e) => JSON.parse(e.data) as IWsMessage<IMessage>,
+        openObserver: {
+          next: () => {
+            this.socketOfflineMessages$.subscribe((message: IWsMessage<IMessage>) => {
+              this.socket$.next(message);
+            });
+          },
+        },
+      });
+      this.clientConnection$ = this.socket$.multiplex(
+        () => ({ subscribe: 'connect' }), 
+        () => ({ unsubscribe: 'connect' }), 
+        (message) => message.event === 'connect'
+      );
+      this.chat$ = this.socket$.multiplex(
+        () => ({ subscribe: 'chat' }), 
+        () => ({ unsubscribe: 'chat' }), 
+        (message) => message.event === 'chat'
+      );
+      this.isTyping$ = this.socket$.multiplex(
+        () => ({ subscribe: 'typing' }), 
+        () => ({ unsubscribe: 'typing' }), 
+        (message) => message.event === 'typing'
+      );
+    }
+
+    this.socket$
+      .pipe(
+        retry({
+          count: 1,
+          delay: (error, retryCount) => {
+            console.log(
+              `Attempt ${retryCount}: Error occurred during websocket connection, retrying...`
+            );
+            return of(error).pipe(delay(1000));
+          },
+        }),
+        catchError((err) => {
+          console.error('Error occurred during websocket connection:', err);
+          this.socket$.unsubscribe();
+          return of(null);
+        })
+      )
+      .subscribe();
   }
 
   sendChatMessage(message: string, clientId: string) {
+    if (this.socket$.closed) {
+      this.socketOfflineMessages$.next({ event: 'message', data: { topic: 'chat', message, clientId }});
+      this.connect();
+
+      return;
+    }
+
     this.socket$.next({ event: 'message', data: { topic: 'chat', message, clientId }});
   }
 
@@ -54,15 +107,15 @@ export class ChatService {
     this.socket$.next({ event: 'typing', data: { topic: 'chat', clientId, isTyping } });
   }
   
-  getClientConnection$(): Observable<WsMessage> {
+  getClientConnection$(): Observable<IWsMessage<IChatConnection>> {
     return this.clientConnection$;
   }
   
-  getChatSocket$(): Observable<WsMessage> {
+  getChatSocket$(): Observable<IWsMessage<IMessage[]>> {
     return this.chat$;
   }
   
-  getIsTyping$(): Observable<WsMessage> {
+  getIsTyping$(): Observable<IWsMessage<IChatEvent>> {
     return this.isTyping$;
   }
 }
