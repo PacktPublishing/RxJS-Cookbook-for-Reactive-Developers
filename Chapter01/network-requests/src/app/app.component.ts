90bc1ba121505481863424ea7b9ae15ec69c83ae
Nikola Mitrovic <nikola.mitrovic1313@gmail.com>
2025-01-27 00:31:38 +0100

    Chapter01 - rxjs network requests - Remove unused import.

--- Chapter01/network-requests/src/app/app.component.ts
+++ Chapter01/network-requests/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { MatButtonModule } from '@angular/material/button';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RecipesListComponent } from './components/recipes-list/recipes-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, RecipesListComponent],
+  imports: [RouterOutlet, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
