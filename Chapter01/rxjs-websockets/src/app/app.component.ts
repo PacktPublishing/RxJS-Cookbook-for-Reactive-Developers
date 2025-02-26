2ddb131a4f7191f4de595e4cafb890d59abc36fb
Nikola Mitrovic <nikola.mitrovic1313@gmail.com>
2025-02-26 22:13:37 +0100

    Chapter01 - websockets - Angular 19 upgrade.

--- Chapter01/rxjs-websockets/src/app/app.component.ts
+++ Chapter01/rxjs-websockets/src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { RecipesListComponent } from './components/recipes-list/recipes-list.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, RecipesListComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet, RecipesListComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'rxjs-websockets';
