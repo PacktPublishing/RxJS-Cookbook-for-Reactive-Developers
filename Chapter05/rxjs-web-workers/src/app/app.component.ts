f4dd621a415e32139f438f4c5e102967ce2e829d
Nikola Mitrovic <nikola.mitrovic1313@gmail.com>
2025-02-27 00:15:57 +0100

    Chapter05 - rxjs web workers - Angular 19 upgrade.

--- Chapter05/rxjs-web-workers/src/app/app.component.ts
+++ Chapter05/rxjs-web-workers/src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { bufferCount, distinctUntilChanged, filter, fromEvent, map, share, throttleTime } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'rxjs-web-workers';
